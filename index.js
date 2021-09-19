758d6d1a5ad99714be2c26553a714fcffbdc02d3
Shubham6697 <sg3477628@gmail.com>
2021-09-19 19:49:42 +0530

    Work in progress

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let answer;
 
 const message = document.querySelector('.message');
 const scoreDOM = document.querySelector('.score');
-//const numberDOM = document.querySelector('.number');
 
 let score = 20;
 let highscore = 0;
@@ -17,15 +16,15 @@ function handleGuessClick(event) {
     let guess = document.querySelector(".number").value;
     //console.log(answer);
 
-    if(answer === undefined)
+    
+    if(answer === undefined)    // When no range select
         alert("Please choose a range");
-    else if(guess === "")
+    else if(guess === "")   // When no input given
         alert("Please enter a number");
-    else if(parseInt(guess) === answer){
+    else if(parseInt(guess) === answer){    // When players wins
         
         message.textContent = "🎉 Great job! That's the correct number!";
 
-        //numberDOM.textContent = answer;
         document.querySelector("body").style.backgroundColor = "rgb(50, 73, 73)";
 
         if(score > highscore){
@@ -33,14 +32,22 @@ function handleGuessClick(event) {
             document.querySelector(".highscore").textContent = highscore;
         }
     } 
-    else if(guess !== answer){
-        if(score > 1){
-            message.textContent = guess > answer ? "📈 Too High!" : "📉 Too Low!";
+    else if(guess !== answer){  // When guess is high or low
+
+        if(score >= 1){
+            
+            if (guess > answer && (guess - answer) <= 3 ) 
+                document.querySelector(".message").textContent = "🤏 You are close! Keep going ⬇";
+            else if(Math.abs(answer - guess) <= 3)
+                document.querySelector(".message").textContent = "🤏 You are close! Keep going ⬆";
+            else
+                message.textContent = guess > answer ? "📈 Too High!" : "📉 Too Low!";    
+            
             score--;
             scoreDOM.textContent = score;
         } 
         else{
-            message.textContent = "😭 You Lost The Round...";
+            message.textContent = "😭 You Lost(❌) The Round...";
             scoreDOM.textContent = 0;
         }
   }
@@ -55,29 +62,50 @@ let range50Button = document.querySelector(".range50");
 let range100Button = document.querySelector(".range100");
 
 function handleRange10Click(event) {
-  range10Button.classList.add("selected");
-  range50Button.classList.remove("selected");
-  range100Button.classList.remove("selected");
-  range = 10;
-  answer = Math.floor(Math.random() * range);
+    range10Button.classList.add("selected");
+    range50Button.classList.remove("selected");
+    range100Button.classList.remove("selected");
+    range = 10;
+    answer = Math.floor(Math.random() * range);
 }
 
 function handleRange50Click(event) {
-  range10Button.classList.remove("selected");
-  range50Button.classList.add("selected");
-  range100Button.classList.remove("selected");
-  range = 50;
-  answer = Math.floor(Math.random() * range);
+    range10Button.classList.remove("selected");
+    range50Button.classList.add("selected");
+    range100Button.classList.remove("selected");
+    range = 50;
+    answer = Math.floor(Math.random() * range);
 }
 
 function handleRange100Click(event) {
-  range10Button.classList.remove("selected");
-  range50Button.classList.remove("selected");
-  range100Button.classList.add("selected");
-  range = 100;
-  answer = Math.floor(Math.random() * range);
+    range10Button.classList.remove("selected");
+    range50Button.classList.remove("selected");
+    range100Button.classList.add("selected");
+    range = 100;
+    answer = Math.floor(Math.random() * range);
 }
 
 range10Button.addEventListener("click", handleRange10Click);
 range50Button.addEventListener("click", handleRange50Click);
 range100Button.addEventListener("click", handleRange100Click);
+
+//Reset button
+document.querySelector(".reset").addEventListener("click", function () {
+    score = 20;
+    answer = Math.trunc(Math.random() * 20) + 1;
+  
+    document.querySelector(".message").textContent = "Start guessing...";
+  
+    document.querySelector(".score").textContent = score;
+  
+    document.querySelector(".number").value = "";
+
+    range50Button.classList.remove("selected");
+
+    range100Button.classList.remove("selected");
+
+    range10Button.classList.remove("selected");
+  
+    document.querySelector("body").style.backgroundColor = "#222";
+  
+});
